10a3d737546972bc0427832080ebbc6b4cc57f03
henriquejhansen <henriquejhansen@gmail.com>
2025-11-04 18:35:18 -0300

    Solution

--- services/movie.py
+++ services/movie.py
@@ -4,10 +4,31 @@ from django.db.models import QuerySet
 from db.models import Movie
 
 
+@transaction.atomic
+def create_movie(
+        movie_title: str,
+        movie_description: str,
+        genres_ids: list[int] = None,
+        actors_ids: list[int] = None
+) -> Movie:
+    movie = Movie.objects.create(
+        title=movie_title,
+        description=movie_description
+    )
+
+    if genres_ids:
+        movie.genres.set(genres_ids)
+
+    if actors_ids:
+        movie.actors.set(actors_ids)
+
+    return movie
+
+
 def get_movies(
-    genres_ids: list[int] = None,
-    actors_ids: list[int] = None,
-    title: str = None,
+        genres_ids: list[int] = None,
+        actors_ids: list[int] = None,
+        title: str = None
 ) -> QuerySet[Movie]:
     queryset = Movie.objects.all()
 
@@ -20,27 +41,4 @@ def get_movies(
     if title:
         queryset = queryset.filter(title__icontains=title)
 
-    return queryset
-
-
-def get_movie_by_id(movie_id: int) -> Movie:
-    return Movie.objects.get(id=movie_id)
-
-
-@transaction.atomic
-def create_movie(
-    movie_title: str,
-    movie_description: str,
-    genres_ids: list = None,
-    actors_ids: list = None,
-) -> Movie:
-    movie = Movie.objects.create(
-        title=movie_title,
-        description=movie_description,
-    )
-    if genres_ids:
-        movie.genres.set(genres_ids)
-    if actors_ids:
-        movie.actors.set(actors_ids)
-
-    return movie
+    return queryset.distinct().order_by("id")
